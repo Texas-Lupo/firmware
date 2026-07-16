b9a270fdd71326ccd0d195ffaa780dec97c663c0
Texas-Lupo <vulpisfoglia.anon@gmail.com>
2026-07-16 11:37:32 +0300

    Update waybeam.mk

--- general/package/waybeam/waybeam.mk
+++ general/package/waybeam/waybeam.mk
@@ -14,24 +14,23 @@ WAYBEAM_SOC ?= star6e
 
 # Override the cross-compiler provided by the Makefile's internal toolchain downloader
 # Ensure we build the main target, json_cli, and regscan as defined in the source
-define WAYBEAM_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="$(TARGET_LDFLAGS) -s" \
-		SOC_BUILD=$(WAYBEAM_SOC) \
-		-C $(@D) \
-		build json_cli regscan
-endef
+#define WAYBEAM_BUILD_CMDS
+#	$(MAKE) CC="$(TARGET_CC)" \
+#		CFLAGS="$(TARGET_CFLAGS)" \
+#		LDFLAGS="$(TARGET_LDFLAGS) -s" \
+#		SOC_BUILD=$(WAYBEAM_SOC) \
+#		-C $(@D) \
+#		build json_cli regscan
+#endef
 
 # Install the built binaries from the SOC-specific output directory to the target[cite: 1, 2]
 define WAYBEAM_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/out/$(WAYBEAM_SOC)/waybeam
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(WAYBEAM_PKGDIR)/files/waybeam
   $(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(WAYBEAM_PKGDIR)/files/waybeam.json
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(WAYBEAM_PKGDIR)/files/S99waybeam
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/out/$(WAYBEAM_SOC)/json_cli
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/out/$(WAYBEAM_SOC)/regscan
+
 endef
 
 $(eval $(generic-package))
